d6be2d794778cf756612323ed4f92fcc9b28cc11
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2025-07-07 21:20:20 +1000

    handleUserRegistration works, we can generate uuid and put user info in dynamodb now :P

--- src/handleUserRegistration.ts
+++ src/handleUserRegistration.ts
@@ -1,12 +1,45 @@
 import { SNSEvent } from "aws-lambda";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import {v4 as uuid}from "uuid"
+import { UserItem } from "./types";
+import { marshall } from "@aws-sdk/util-dynamodb";
 
+
+const db = new DynamoDBClient({});
+const TABLE_NAME = process.env.USERS_TABLE;
 
 export const handler = async (event:SNSEvent) =>{
-    console.log("test")
-    try{
-        console.log(event.Records)
-    }catch(err:any){
-        console.error("handleUserRegistration record error:",err);
-        throw err
+    for (const record of event.Records){
+        try{
+            //parse name and email from sns
+            const payload = JSON.parse(record.Sns.Message) as {
+                name: string;
+                email: string;
+              };
+            const { name, email } = payload;
+
+            //create userId
+            const userId = uuid()
+            const newUser:UserItem = {
+                userId,
+                name,
+                email,
+                createdAt: new Date().toISOString(),
+            }
+
+            await db.send(
+                new PutItemCommand({
+                    TableName: TABLE_NAME,
+                    Item: marshall(newUser),
+                })
+            )
+            console.log(`Created user ${userId} (email: ${email}, name:${name})`)
+
+
+        }catch(err:any){
+            console.error("handleUserRegistration record error:",err);
+            throw err
+        }
     }
+   
 }
